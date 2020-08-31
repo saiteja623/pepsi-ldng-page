0ed73a1c610798f370b4fc20a79e252ae09e240a
saiteja623 <saiteja.balla13@gmail.com>
2020-08-31 22:32:34 +0530

    Updates

---
+++ precache-manifest.74eeb42c9d7fbfb58f0310eef3b3c7c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4827d6bcf01964285e12860cbb5e47",
+    "url": "/pepsi-ldng-page/index.html"
+  },
+  {
+    "revision": "3e53d483bf0f4aadf730",
+    "url": "/pepsi-ldng-page/static/css/main.47517580.chunk.css"
+  },
+  {
+    "revision": "30dec47ab16172d473c1",
+    "url": "/pepsi-ldng-page/static/js/2.a7161642.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pepsi-ldng-page/static/js/2.a7161642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e53d483bf0f4aadf730",
+    "url": "/pepsi-ldng-page/static/js/main.5bb895c7.chunk.js"
+  },
+  {
+    "revision": "94611e2d126f509fa8ff",
+    "url": "/pepsi-ldng-page/static/js/runtime-main.13386f34.js"
+  },
+  {
+    "revision": "fc7d3155f65b3dbdae5a6a9faff93dd0",
+    "url": "/pepsi-ldng-page/static/media/pepsi001.fc7d3155.png"
+  },
+  {
+    "revision": "671ffb98d7d965791a55949a584a96d1",
+    "url": "/pepsi-ldng-page/static/media/pepsi002.671ffb98.png"
+  },
+  {
+    "revision": "6eeb577ec4105dfefee281723411e9e2",
+    "url": "/pepsi-ldng-page/static/media/pepsi003.6eeb577e.png"
+  }
+]);
